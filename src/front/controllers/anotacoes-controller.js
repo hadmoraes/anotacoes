612d00b269ceaa2999cf3e8a1c0c91e1d57013d2
Hadassa Moraes <had.moraes@gmail.com>
2022-02-08 20:45:35 -0300

    add anotacoes-controller.js

---
+++ src/front/controllers/anotacoes-controller.js
@@ -0,0 +1,22 @@
+class AnotacoesController{
+    constructor(){
+        this.arrAnotacoes = [];
+    }
+    addAnotacao(){
+        const titulo= $('#titulo').val();
+        const notas = $('#descricaoTarefa').val();
+        const anotacao = new Anotacoes(titulo,notas);
+        this.arrAnotacoes.push(anotacao);
+        console.log(this.arrAnotacoes, anotacao);
+    }
+}
+
+
+const btn = $('#btnSubmit')
+
+btn.on('click', (event)=>{
+    event.preventDefault();
+    const anotacaoNova = new AnotacoesController();
+    anotacaoNova.addAnotacao()
+    console.log('oi')
+})
