fdf844158e300fd7a909fd9c796f994c45567a4a
Hadassa Moraes <had.moraes@gmail.com>
2022-02-09 22:22:11 -0300

    adicionou instancia de AnotacoesView em anotacoes-controller.js

--- src/front/controllers/anotacoes-controller.js
+++ src/front/controllers/anotacoes-controller.js
@@ -1,13 +1,21 @@
 class AnotacoesController{
     constructor(){
         this.arrAnotacoes = [];
+        this.mostraAnotacao = new AnotacoesView();
     }
+
+    getArrAnotacoes(){
+        return this.arrAnotacoes
+    }
+
     addAnotacao(){
         const titulo= $('#titulo').val();
         const notas = $('#descricaoTarefa').val();
         const anotacao = new Anotacoes(titulo,notas);
         this.arrAnotacoes.push(anotacao);
+        this.mostraAnotacao.renderizaAnotacao(anotacao);
     }
+
 }
 
 
@@ -15,6 +23,7 @@ const btn = $('#btnSubmit')
 
 btn.on('click', (event)=>{
     event.preventDefault();
-    const anotacaoNova = new AnotacoesController();
-    anotacaoNova.addAnotacao();
+    const criaAnotacao = new AnotacoesController();
+    criaAnotacao.addAnotacao();
+    console.log(criaAnotacao);
 })
